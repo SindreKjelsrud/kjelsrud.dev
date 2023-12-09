d9dfec6dbc7dee26d35178091896b152c743d8cd
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-10 00:17:01 +0100

    :bug: fix rss bugs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,9 @@
 import { defineCollection, z } from 'astro:content';
+import { rssSchema } from '@astrojs/rss';
 
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val))
-	}),
+	schema: rssSchema
 });
 
 export const collections = { blog };
