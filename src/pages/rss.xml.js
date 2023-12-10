8a32c3bf0cfdda8b0d44794a48400a98783f2266
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-10 02:28:43 +0100

    :sparkles: added RSS to site

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: 'Sindre Kjelsrud',
+    description: 'Recent content on Sindre Kjelsrud`s blog',
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: post.data.customData,
+      link: `/blog/${post.slug}/`,
+    })),
+  });
+}
