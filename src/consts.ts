f44f06b949bfd15595bed23e749f8a7f34c1e004
Sindre Kjelsrud <kjelsrudsindre@gmail.com>
2023-12-10 01:11:19 +0100

    :tophat: fix helmet

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'kjelsrud.dev';
+export const SITE_TITLE = 'Sindre Kjelsrud';
 export const SITE_DESCRIPTION = 'kjelsrud.dev';
